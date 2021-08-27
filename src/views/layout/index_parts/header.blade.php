0b7f603142c3382dc4fad258f19526690fb4ece9
yudijohn <yudi@javateknologi.com>
2021-08-27 21:22:19 +0700

    Adding plugins folder and improving header

--- src/views/layout/index_parts/header.blade.php
+++ src/views/layout/index_parts/header.blade.php
@@ -52,7 +52,7 @@
           <div class="d-flex align-items-center ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
             <!--begin::Menu wrapper-->
             <div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end" data-kt-menu-flip="bottom">
-              <img src="assets/media/avatars/150-26.jpg" alt="metronic" />
+              <img alt="Avatar" src="{{ asset( 'plugins/yudijohn/metronic/img/avatar.png' ) }}" />
             </div>
             <!--begin::Menu-->
             <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
@@ -61,7 +61,7 @@
                 <div class="menu-content d-flex align-items-center px-3">
                   <!--begin::Avatar-->
                   <div class="symbol symbol-50px me-5">
-                    <img alt="Logo" src="assets/media/avatars/150-26.jpg" />
+                    <img alt="Avatar" src="{{ asset( 'plugins/yudijohn/metronic/img/avatar.png' ) }}" />
                   </div>
                   <!--end::Avatar-->
                   <!--begin::Username-->
@@ -80,67 +80,6 @@
               <!--begin::Menu item-->
               <div class="menu-item px-5">
                 <a href="../../demo1/dist/account/overview.html" class="menu-link px-5">My Profile</a>
-              </div>
-              <!--end::Menu item-->
-              <!--begin::Menu item-->
-              <div class="menu-item px-5">
-                <a href="../../demo1/dist/pages/projects/list.html" class="menu-link px-5">
-                  <span class="menu-text">My Projects</span>
-                  <span class="menu-badge">
-                    <span class="badge badge-light-danger badge-circle fw-bolder fs-7">3</span>
-                  </span>
-                </a>
-              </div>
-              <!--end::Menu item-->
-              <!--begin::Menu item-->
-              <div class="menu-item px-5" data-kt-menu-trigger="hover" data-kt-menu-placement="left-start" data-kt-menu-flip="bottom">
-                <a href="#" class="menu-link px-5">
-                  <span class="menu-title">My Subscription</span>
-                  <span class="menu-arrow"></span>
-                </a>
-                <!--begin::Menu sub-->
-                <div class="menu-sub menu-sub-dropdown w-175px py-4">
-                  <!--begin::Menu item-->
-                  <div class="menu-item px-3">
-                    <a href="../../demo1/dist/account/referrals.html" class="menu-link px-5">Referrals</a>
-                  </div>
-                  <!--end::Menu item-->
-                  <!--begin::Menu item-->
-                  <div class="menu-item px-3">
-                    <a href="../../demo1/dist/account/billing.html" class="menu-link px-5">Billing</a>
-                  </div>
-                  <!--end::Menu item-->
-                  <!--begin::Menu item-->
-                  <div class="menu-item px-3">
-                    <a href="../../demo1/dist/account/statements.html" class="menu-link px-5">Payments</a>
-                  </div>
-                  <!--end::Menu item-->
-                  <!--begin::Menu item-->
-                  <div class="menu-item px-3">
-                    <a href="../../demo1/dist/account/statements.html" class="menu-link d-flex flex-stack px-5">Statements
-                    <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="View your statements"></i></a>
-                  </div>
-                  <!--end::Menu item-->
-                  <!--begin::Menu separator-->
-                  <div class="separator my-2"></div>
-                  <!--end::Menu separator-->
-                  <!--begin::Menu item-->
-                  <div class="menu-item px-3">
-                    <div class="menu-content px-3">
-                      <label class="form-check form-switch form-check-custom form-check-solid">
-                        <input class="form-check-input w-30px h-20px" type="checkbox" value="1" checked="checked" name="notifications" />
-                        <span class="form-check-label text-muted fs-7">Notifications</span>
-                      </label>
-                    </div>
-                  </div>
-                  <!--end::Menu item-->
-                </div>
-                <!--end::Menu sub-->
-              </div>
-              <!--end::Menu item-->
-              <!--begin::Menu item-->
-              <div class="menu-item px-5">
-                <a href="../../demo1/dist/account/statements.html" class="menu-link px-5">My Statements</a>
               </div>
               <!--end::Menu item-->
               <!--begin::Menu separator-->
