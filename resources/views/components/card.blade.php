865cad9828b1299b01f706aa52fde0dd9e404526
yudijohn <yudi@javateknologi.com>
2023-03-17 20:24:43 +0700

    Adding card header component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,22 +1,28 @@
 <div {{ $attributes->class( [ 'card card-flush' ] ) }}>
-    @if( isset( $title ) )
-        <!--begin::Header-->
-        <div class="card-header pt-5">
-            <!--begin::Title-->
-            <h3 class="card-title align-items-start flex-column">
-                <span class="card-label fw-bold text-gray-800">{{ $title }}</span>
-                @if( isset( $subtitle ) )
-                    <span class="text-gray-400 mt-1 fw-semibold fs-6">{{ $subtitle }}</span>
-                @endif
-            </h3>
-            <!--end::Title-->
-            @if( isset( $toolbar ) )
-                <!--begin::Toolbar-->
-                <div class="card-toolbar">{{ $toolbar }}</div>
-                <!--end::Toolbar-->
-            @endif
+    @if( isset( $header ) )
+        <div {{ $header->attributes->class( [ 'card-header pt-5' ] ) }}>
+            {{ $header }}
         </div>
-        <!--end::Header-->
+    @else
+        @if( isset( $title ) )
+            <!--begin::Header-->
+            <div class="card-header pt-5">
+                <!--begin::Title-->
+                <h3 class="card-title align-items-start flex-column">
+                    <span class="card-label fw-bold text-gray-800">{{ $title }}</span>
+                    @if( isset( $subtitle ) )
+                        <span class="text-gray-400 mt-1 fw-semibold fs-6">{{ $subtitle }}</span>
+                    @endif
+                </h3>
+                <!--end::Title-->
+                @if( isset( $toolbar ) )
+                    <!--begin::Toolbar-->
+                    <div class="card-toolbar">{{ $toolbar }}</div>
+                    <!--end::Toolbar-->
+                @endif
+            </div>
+            <!--end::Header-->
+        @endif
     @endif
     <!--begin::Body-->
     @if( isset( $body ) )
