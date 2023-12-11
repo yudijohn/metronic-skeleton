a9a2526a1daa1e21c27e0dd2b88340904a497620
yudijohn <yudi@javateknologi.com>
2023-12-11 09:10:02 +0700

    Adding publishing file group

--- src/MetronicServiceProvider.php
+++ src/MetronicServiceProvider.php
@@ -30,14 +30,16 @@ class MetronicServiceProvider extends ServiceProvider
         // $this->loadMigrationsFrom( __DIR__.'/database/migrations' );
         $this->loadViewsFrom( __DIR__.'/../resources/views', 'metronic' );
         $this->publishes( [
+            __DIR__.'/plugins' => base_path( 'public/plugins/yudijohn/metronic' ),
+        ], 'plugins' );
+        $this->publishes( [
             // __DIR__.'/Resources' => base_path( 'app/Http/Resources' ),
             // __DIR__.'/database/migrations' => base_path( 'database/migrations' ),
-            __DIR__.'/plugins' => base_path( 'public/plugins/yudijohn/metronic' ),
             __DIR__.'/../resources/views/layout/index_parts/aside.blade.php' => resource_path( 'views/vendor/metronic/layout/index_parts/aside.blade.php' ),
             __DIR__.'/../resources/views/components/icons' => resource_path( 'views/vendor/metronic/components/icons' ),
             __DIR__.'/../config/system.php' => config_path( 'system.php' ),
             __DIR__.'/../resources/lang' => resource_path( 'lang/vendor/metronic' ),
-        ] );
+        ], 'resources' );
         $this->app[ 'router' ]->pushMiddlewareToGroup( 'web', Middleware\CustomForms::class );
         $this->loadRoutesFrom( __DIR__.'/../routes/web.php' );
         $this->loadRoutesFrom( __DIR__.'/../routes/admin.php' );
