5a964b729b960c381e5e5c0f190d83e2ceb2937f
yudijohn <yudijohn.sg@gmail.com>
2023-12-15 14:04:37 +0700

    Update page title

--- resources/views/layout/index_parts/header.blade.php
+++ resources/views/layout/index_parts/header.blade.php
@@ -28,7 +28,7 @@
       <div data-kt-swapper="true" data-kt-swapper-mode="{default: 'prepend', lg: 'prepend'}" data-kt-swapper-parent="{default: '#kt_app_content_container', lg: '#kt_app_header_wrapper'}" class="page-title d-flex flex-column justify-content-center flex-wrap me-3 mb-5 mb-lg-0">
         <!--begin::Title-->
         <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0">
-          Page Title
+          @yield( 'page_title', config( 'app.name' ) )
         </h1>
         <!--end::Title-->
       </div>
